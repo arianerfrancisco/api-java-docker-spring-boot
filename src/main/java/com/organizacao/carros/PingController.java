658c60c3ebc7a80d51f56b8f06402c11c3fc9e34
Ariane <arianerochaf@gmail.com>
2022-09-10 12:09:11 -0300

    finalizacao projeto

--- src/main/java/com/organizacao/carros/PingController.java
+++ src/main/java/com/organizacao/carros/PingController.java
@@ -14,5 +14,8 @@ public class PingController {
     @ResponseStatus(HttpStatus.OK)
     public void ping(){
         System.out.println("Tudo funcionando");
+        System.out.println("LiveReload funcionando!!!!!");
+        System.out.println("LiveReload funcionando!!!!!");
+
     }
 }
